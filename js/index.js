09508bef7fd2b994eb82220d43d591566b3887d2
Greg Roberts <gregaroberts511@gmail.com>
2020-10-04 16:45:41 -0700

    2 event listeners complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+let busPic = document.querySelector(".intro img");
+
+//1
+busPic.addEventListener("mouseenter", (e) => {
+  busPic.style.transform = "scale(2)";
+  busPic.style.transition = "transform 1s";
+});
+
+//2
+busPic.addEventListener("mouseleave", (e) => {
+  busPic.style.transform = "scale(1)";
+  busPic.style.transition = "transform 1s";
+});
+
+let nav =
