ded44e7f31532595b13e19ec301d3fa37a04f520
Greg Roberts <gregaroberts511@gmail.com>
2020-10-04 16:51:37 -0700

    added nav bar event listeners

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let busPic = document.querySelector(".intro img");
 
 //1
 busPic.addEventListener("mouseenter", (e) => {
-  busPic.style.transform = "scale(2)";
+  busPic.style.transform = "scale(2.5)";
   busPic.style.transition = "transform 1s";
 });
 
@@ -14,4 +14,19 @@ busPic.addEventListener("mouseleave", (e) => {
   busPic.style.transition = "transform 1s";
 });
 
-let nav =
+let nav = document.querySelector(".main-navigation");
+//3
+
+nav.addEventListener("click", (e) => {
+  nav.style.backgroundColor = "grey";
+});
+
+//4
+nav.addEventListener("dblclick", (e) => {
+  nav.style.backgroundColor = "red";
+});
+
+//5
+nav.addEventListener("mouseleave", (e) => {
+  nav.style.backgroundColor = "white";
+});
